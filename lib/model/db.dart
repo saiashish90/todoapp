f2e06a399652d09a8e8de526cdfaaac60d4ff02f
Sai Ashish <33771241+saiashish90@users.noreply.github.com>
2019-11-12 10:16:00 +0530

    .

--- lib/model/db.dart
+++ lib/model/db.dart
@@ -2,7 +2,6 @@ import 'dart:io';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
-
 import 'package:flutter_todos/model/model.dart';
 
 const kTodosStatusActive = 0;
